150c9ec0974afb3e26a894903f49136c9313bf48
UNIQLSET <124276322+UNIQLSET@users.noreply.github.com>
2026-09-07 11:41:41 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+
+const CACHE = 'schedule-3course-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached || fetch(event.request).then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(c => c.put(event.request, copy));
+        return response;
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
